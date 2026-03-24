f89f2ae2576950cdccacf22c0a7d8f2ff95427c7
JesusC <jesuscaraballo24@gmail.com>
2026-03-24 16:48:55 -0300

    otro commit

--- sql/V2__insert_data.sql
+++ sql/V2__insert_data.sql
@@ -1,5 +1,5 @@
 INSERT INTO customers (id, name, email)
 VALUES
-    (1, 'Juan Perez', 'juan.perez@email.com'),
-    (2, 'Maria Lopez', 'maria.lopez@email.com'),
-    (3, 'Carlos Ruiz', 'carlos.ruiz@email.com');
+(1, 'Juan Perez', 'juan.perez@email.com'),
+(2, 'Maria Lopez', 'maria.lopez@email.com'),
+(3, 'Carlos Ruiz', 'carlos.ruiz@email.com');
--- sql/V4__create_table_orders.sql
+++ sql/V4__create_table_orders.sql
@@ -2,5 +2,5 @@ CREATE TABLE orders (
     id SERIAL PRIMARY KEY,
     customer_id INT NOT NULL,
     order_date DATE NOT NULL,
-    total NUMERIC(10,2) NOT NULL
+    total NUMERIC(10, 2) NOT NULL
 );
