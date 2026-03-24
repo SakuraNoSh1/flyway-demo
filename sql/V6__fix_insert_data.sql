1a21cd021e509c7cb6da7592221da9a88b1e1b40
JesusC <jesuscaraballo24@gmail.com>
2026-03-24 17:21:50 -0300

    eliminacion de tablas

--- sql/V6__fix_insert_data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO users (id, username, email)
-VALUES
-(1, 'juan_perez', 'juan.perez@email.com'),
-(2, 'maria_lopez', 'maria.lopez@email.com'),
-(3, 'carlos_ruiz', 'carlos.ruiz@email.com');
